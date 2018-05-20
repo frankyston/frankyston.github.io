1424cee11d826a27fdffe77a829537a7fb36b9cf
Frankyston Lins <frankyston@gmail.com>
2018-05-20 16:28:54 -0300

    configuration Open Graph Facebook

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -36,12 +36,20 @@ module CustomHelpers
 
   def generate_fb_tags
     capture_haml do
+      haml_tag(:meta, property: 'og:type', content: fb_metatag_type)
       haml_tag(:meta, property: 'og:title', content: fb_metatag_title)
       haml_tag(:meta, property: 'og:description', content: fb_metatag_description)
       haml_tag(:meta, property: 'og:url', content: fb_metatag_url)
-      haml_tag(:meta, property: 'og:image', content: fb_metatag_image)
+      haml_tag(:meta, property: 'og:image:url', content: fb_metatag_image)
+      haml_tag(:meta, property: 'og:image:type', content: fb_metatag_image_type)
+      haml_tag(:meta, property: 'og:image:width', content: fb_metatag_image_width)
+      haml_tag(:meta, property: 'og:image:height', content: fb_metatag_image_height)
       haml_tag(:meta, property: 'og:site_name', content: "Frankyston Lins")
     end
+  end
+
+  def fb_metatag_type
+    current_page.data.fb.present? ? "article" : "website"
   end
 
   def fb_metatag_title
@@ -53,7 +61,19 @@ module CustomHelpers
   end
 
   def fb_metatag_image
-    current_page.data.fb.present? ? "#{current_site_url}/#{current_page.data.fb.image.url}" : "#{current_site_url}/images/foto.png"
+    current_page.data.fb.present? ? "#{current_site_url}#{current_page.data.fb.image.url}" : "#{current_site_url}/images/foto.png"
+  end
+
+  def fb_metatag_image_type
+    current_page.data.fb.present? ? current_page.data.fb.image.type : "image/png"
+  end
+  
+  def fb_metatag_image_width
+    current_page.data.fb.present? ? current_page.data.fb.image.width : 300
+  end
+  
+  def fb_metatag_image_height
+    current_page.data.fb.present? ? current_page.data.fb.image.height : 400
   end
 
   def fb_metatag_description
